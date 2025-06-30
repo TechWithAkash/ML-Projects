ddd396555b52d5d9b1475f3be401c4023bd5a27a
TechWithAkash <vishwakarmaakashav17@gmail.com>
2025-06-30 23:52:22 +0530

    Deployment Configuration added

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
+
+app = application
 
 # Route for the Home Page
 @app.route("/")
